f7619e8d5f90a04dd59ce2f04f4923c7129351a0
sebastian.rossetti <sebastian.rossetti@piano.io>
2025-03-11 23:00:40 +0100

    Applied fixes

--- main.py
+++ main.py
@@ -33,11 +33,16 @@ def read_first_file_in_folder(
 
 def read_data():
     data = read_first_file_in_folder(folder_name=SETTINGS.DATA_FOLDER)
+    data = data.assign(UPC=data["UPC"].str.split(",")).explode("UPC")
+    data["UPC"] = pandas.to_numeric(data["UPC"], errors="coerce")
+    data["UPC"] = data["UPC"].round().astype(int)
+    data = data.drop_duplicates(subset=["ID", "UPC"]).reset_index(drop=True)
     return data
 
 
 def read_lookup() -> dict[str, int]:
     data = read_first_file_in_folder(folder_name=SETTINGS.LOOKUP_FOLDER)
+    data = data.groupby("UPC").agg({"Quantity": "sum"}).reset_index()
     output = {}
     for index, row in data.iterrows():
         output[row["UPC"]] = int(row["Quantity"])
@@ -46,9 +51,25 @@ def read_lookup() -> dict[str, int]:
 
 def save_output(data: list[OutputEntry]):
     output_df = pandas.DataFrame(data)
-    output_df.to_csv(
-        f"{SETTINGS.OUTPUT_FOLDER}/{SETTINGS.TIMESTAMP}_output.csv", index=False
-    )
+    filename = f"{SETTINGS.OUTPUT_FOLDER}/{SETTINGS.TIMESTAMP}_output.csv"
+
+    def format_currency(value):
+        if value > 0:
+            return f"${value:.2f}"
+        elif value < 0:
+            return f"(${abs(value):.2f})"
+        else:
+            return "$-"
+
+    for column in OutputEntry.get_currency_fields():
+        output_df[column] = output_df[column].apply(format_currency)
+    output_df["UPC"] = "'" + output_df["UPC"].astype(str)
+    output_df["Stock Code"] = "'" + output_df["Stock Code"].astype(str)
+    output_df.to_csv(filename, index=False)
+
+
+def lookup_upc(upc: str, data: pandas.DataFrame, column: str = "UPC") -> pandas.DataFrame:
+    return data[data[column] == upc]
 
 
 def main():
@@ -57,22 +78,23 @@ def main():
 
     output = []
     for upc, quantity in lookup.items():
-        entry = OutputEntry()
-        entry.upc = str(upc)
-        entry.qty_input = quantity
-
-        results = data[data["UPC"] == upc]
+        results = lookup_upc(upc=upc, data=data)
         if len(results) > 1:
             raise ValueError(f"Multiple results found for UPC: {upc}")
 
-        if not results.empty:
-            result = results.iloc[0]
-            entry.qty_database = int(result["Qty On Hand"])
-            entry.unit_cost = float(result["Unit Cost"])
-            entry.stock_code = result["Stock Code"]
-            entry.name = result["Name"]
-            entry.category = result["Category Name"]
-            entry.category_group = result["Category Group Name"]
+        if results.empty:
+            continue
+
+        entry = OutputEntry()
+        entry.upc = str(upc)
+        entry.qty_input = quantity
+        result = results.iloc[0]
+        entry.qty_database = int(float(result["Total Qty On Hand"]))
+        entry.unit_cost = float(result["Latest Cost"])
+        entry.stock_code = result["Stock Code"]
+        entry.name = result["Name"]
+        entry.category = result["Category Name"]
+        entry.category_group = result["Category Group Name"]
 
         entry.unit_variance = entry.qty_input - entry.qty_database
         entry.dollar_variance = round(entry.unit_variance * entry.unit_cost, 2)
--- src/OutputEntry.py
+++ src/OutputEntry.py
@@ -5,12 +5,20 @@ from pydantic import BaseModel, Field
 
 class OutputEntry(BaseModel):
     upc: Optional[str] = Field(default="", alias="UPC")
+    qty_input: Optional[int] = Field(default=0, alias="Quantity Input")
+    qty_database: Optional[int] = Field(default=0, alias="Quantity Database")
+    unit_cost: Optional[float] = Field(default=0, alias="Unit Cost", description="Currency")
     stock_code: Optional[str] = Field(default="", alias="Stock Code")
-    qty_input: Optional[int] = Field(default=0, alias="Qty Counted")
-    name: Optional[str] = Field(default="", alias="Item Name")
-    unit_cost: Optional[float] = Field(default=0, alias="Unit Cost")
-    category: Optional[str] = Field(default="", alias="Category Name")
-    category_group: Optional[str] = Field(default="", alias="Category Group Name")
-    qty_database: Optional[int] = Field(default=0, alias="Expected Qty in POS")
-    unit_variance: Optional[int] = Field(default=0, alias="Variance in Units")
-    dollar_variance: Optional[float] = Field(default=0, alias="Variance in Dollars")
+    name: Optional[str] = Field(default="", alias="Name")
+    category: Optional[str] = Field(default="", alias="Category")
+    category_group: Optional[str] = Field(default="", alias="Category Group")
+    unit_variance: Optional[int] = Field(default=0, alias="Unit Variance")
+    dollar_variance: Optional[float] = Field(default=0, alias="Dollar Variance", description="Currency")
+
+    @staticmethod
+    def get_currency_fields() -> list[str]:
+        return [
+            field_info.alias
+            for field_name, field_info in OutputEntry.model_fields.items()
+            if field_info.description == "Currency"
+        ]
